7ac4538b5349ae5e96f93dedf861a4cf55cf2036
Ray Strode <rstrode@redhat.com>
2010-10-16 19:36:52 -0400

    lib: add private header
    I forgot to add it in commit 8c031bce3f6fe67bd5f0f782a457aebd6af0ceba

---
+++ src/libaccountsservice/act-user-private.h
@@ -0,0 +1,45 @@
+/* -*- Mode: C; tab-width: 8; indent-tabs-mode: nil; c-basic-offset: 8 -*-
+ *
+ * Copyright (C) 2004-2005 James M. Cape <jcape@ignore-your.tv>.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+/*
+ * Private interfaces to the ActUser object
+ */
+
+#ifndef __ACT_USER_PRIVATE_H_
+#define __ACT_USER_PRIVATE_H_
+
+#include <pwd.h>
+
+#include "act-user.h"
+
+G_BEGIN_DECLS
+
+void           _act_user_update_from_object_path   (ActUser    *user,
+                                                    const char *object_path);
+void           _act_user_update_login_frequency    (ActUser    *user,
+                                                    guint64     login_frequency);
+
+void           _act_user_add_session      (ActUser             *user,
+                                           const char          *session_id);
+void           _act_user_remove_session   (ActUser             *user,
+                                           const char          *session_id);
+
+G_END_DECLS
+
+#endif /* !__ACT_USER_PRIVATE_H_ */
