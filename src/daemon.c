magcius/accountsservice

commit eb9ad71d535401e1cb60ece46fb8772ee5e91281

daemon: FIx overly aggressive user exclusion
commit 046998a3a5305f381e6e39bc4bf7f69f260856f4 introduced
a fix that forces users without shells to be excluded.

This fix has made the function return "excluded" if the passed
in shell was NULL.  The code passes NULL for the shell in various
places to mean don't check the shell, though, this commit broke
listing users.

src/daemon.c
@@ -164,7 +164,7 @@ daemon_local_user_is_excluded (Daemon *daemon, const gchar *username, const gcha
         char *basename, *nologin_basename, *false_basename;
         int ret;
 
-        if (shell == NULL || shell[0] == '\0') {
+        if (shell != NULL && shell[0] == '\0') {
                 return TRUE;
         }
 

commit f879ee5e6e16766c291fe38420cd8a33273b0afa

daemon: Fix spurious log spew
commit eb9ad71d535401e1cb60ece46fb8772ee5e91281 introduced a bug
leading to scary looking log message:

Glib-CRITICAL **: g_path_get_basename: assertion 'filename != NULL' failed

This commit reworks that function, to ensure that when shell is NULL,
we never try to take its basename.

src/daemon.c
@@ -161,29 +161,33 @@ error_get_type (void)
 gboolean
 daemon_local_user_is_excluded (Daemon *daemon, const gchar *username, const gchar *shell)
 {
-        char *basename, *nologin_basename, *false_basename;
         int ret;
 
-        if (shell != NULL && shell[0] == '\0') {
+        if (g_hash_table_lookup (daemon->priv->exclusions, username)) {
                 return TRUE;
         }
 
         ret = FALSE;
-        basename = g_path_get_basename (shell);
-        nologin_basename = g_path_get_basename (PATH_NOLOGIN);
-        false_basename = g_path_get_basename (PATH_FALSE);
 
-        if (g_strcmp0 (basename, nologin_basename) == 0) {
-                ret = TRUE;
-        } else if (g_strcmp0 (basename, false_basename) == 0) {
-                ret = TRUE;
-        } else if (g_hash_table_lookup (daemon->priv->exclusions, username)) {
-                ret = TRUE;
+        if (shell != NULL) {
+                char *basename, *nologin_basename, *false_basename;
+
+                basename = g_path_get_basename (shell);
+                nologin_basename = g_path_get_basename (PATH_NOLOGIN);
+                false_basename = g_path_get_basename (PATH_FALSE);
+
+                if (shell[0] == '\0') {
+                        ret = TRUE;
+                } else if (g_strcmp0 (basename, nologin_basename) == 0) {
+                        ret = TRUE;
+                } else if (g_strcmp0 (basename, false_basename) == 0) {
+                        ret = TRUE;
+                }
+
+                g_free (basename);
+                g_free (nologin_basename);
+                g_free (false_basename);
         }
-
-        g_free (basename);
-        g_free (nologin_basename);
-        g_free (false_basename);
 
         return ret;
 }